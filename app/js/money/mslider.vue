8a4eed3ae73c257a87adc1b90db9d5659d0778c6
王嘉婷 <523021368@qq.com>
2018-05-12 17:38:35 +0800

    test

---
+++ app/js/money/mslider.vue
@@ -0,0 +1,42 @@
+<template lang="html">
+    <section :class="$style.content">
+      <Slider :items="items" :cname="$style.slider"/>
+    </section>
+</template>
+
+<script>
+//import Panel from '../core/panel.vue'
+import Slider from '../core/slider.vue'
+export default {
+  components:{
+    Slider,
+  },
+  data(){
+    return {
+      items:[{
+        href: 'money',
+        img: '//img12.360buyimg.com/jrpmobile/jfs/t19087/201/1952941995/49031/f0f213c8/5addc4b1N07ba5c08.jpg?width=750&height=400',
+      },{
+        href: 'money',
+        img: '//img12.360buyimg.com/jrpmobile/jfs/t19690/182/1909914430/49554/80cbbe8b/5addc4c2Nb9b1a20d.jpg?width=750&height=400',
+      },{
+        href: 'money',
+        img: '//img12.360buyimg.com/jrpmobile/jfs/t17044/72/1905017687/57902/9229137c/5addc4d2N28eeac2f.jpg?width=750&height=400',
+      },{
+        href: 'money',
+        img: '//img12.360buyimg.com/jrpmobile/jfs/t17671/227/1922535978/60213/a0585c59/5addc4e2N11bdd0ef.png?width=750&height=400',
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="scss" module>
+  @import "../../css/element.scss";
+
+    .content{
+      .slider{
+        margin-top: 100px;
+      }
+    }
+</style>
---
+++ app/js/raise/rslider.vue
@@ -0,0 +1,68 @@
+<template lang="html">
+  <Panel title="金融头条" :class="$style.panel">
+    <section :class="$style.content">
+      <swiper :options="options">
+        <swiper-slide><p>百万白条券免费送，速来领取！！！！！！！</p><em>></em> </swiper-slide>
+        <swiper-slide><p>签到领流量，1元500M！</p><em>></em> </swiper-slide>
+      </swiper>
+    </section>
+  </Panel>
+</template>
+
+<script>
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
+import Panel from '../core/panel.vue'
+export default {
+  components:{
+    Panel,
+    swiper,
+    swiperSlide,
+  },
+  data(){
+    return {
+      options: {
+        autoplay: true,
+        loop: true,
+        direction: "vertical",
+      },
+    }
+  }
+}
+</script>
+<style lang="scss">
+  .swiper-container-vertical{
+    height: 72px;
+    .swiper-slide{
+      font-family: PingFangSC-Medium;
+      font-size: 28px;
+      color: #333;
+      p{
+        width: 400px;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+        overflow: hidden;
+      }
+      em{
+        color: #ddd;
+        float: right;
+        margin-right: 20px;
+      }
+    }
+  }
+</style>
+<style lang="scss" module>
+  @import "../../css/element.scss";
+  .panel{
+    @include panel;
+    >h4{
+      display: none;
+    }
+    margin-top: 100px!important;
+    .content{
+      height: 72px;
+      line-height: 72px;
+      padding-left: 178px;
+      background: url(//m.jr.jd.com/spe/qyy/main/images/scroll_title_img.png) left top no-repeat
+    }
+  }
+</style>
